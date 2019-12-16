1f01da7ead500239ba8924d3cdfef02dd9ab7bb9
Laurentlsb <laurentlsb06@gmail.com>
2019-12-16 16:01:20 -0600

    add comment

--- leetcode/editor/en/[105]Construct Binary Tree from Preorder and Inorder Traversal.py
+++ leetcode/editor/en/[105]Construct Binary Tree from Preorder and Inorder Traversal.py
@@ -39,5 +39,13 @@ class Solution:
         root.left = self.buildTree(preorder[1:index+1], inorder[:index])
         root.right = self.buildTree(preorder[index+1:], inorder[index+1:])
         return root
-
+"""
+根据前序遍历和中序遍历的性质，重新搭建tree
+方法：分治法（递归），不断构造子树
+1. 前序遍历的第一个为根节点的值
+2. 根据该值在inorder中找根节点的位置，就能分割成左右两个子树
+3. 然后递归，找子树的根节点
+4. 返回root节点
+注意：这里的根节点的索引位置，同样将preorder分割成两个部分，preorder[index+1:]即为右子树的所有节点，前一半的长度等于左子节点加上根节点的个数
+""
 #leetcode submit region end(Prohibit modification and deletion)
