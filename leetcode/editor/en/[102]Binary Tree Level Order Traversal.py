0a68086ccfcb37288abd1ce38b1417ba2ce9e77a
Laurentlsb <laurentlsb06@gmail.com>
2019-12-16 14:09:42 -0600

    suspended

---
+++ leetcode/editor/en/[102]Binary Tree Level Order Traversal.py
@@ -0,0 +1,38 @@
+#Given a binary tree, return the level order traversal of its nodes' values. (ie, from left to right, level by level). 
+#
+# 
+#For example: 
+#Given binary tree [3,9,20,null,null,15,7], 
+# 
+#    3
+#   / \
+#  9  20
+#    /  \
+#   15   7
+# 
+# 
+# 
+#return its level order traversal as: 
+# 
+#[
+#  [3],
+#  [9,20],
+#  [15,7]
+#]
+# 
+# Related Topics Tree Breadth-first Search
+
+
+
+#leetcode submit region begin(Prohibit modification and deletion)
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def levelOrder(self, root: TreeNode) -> List[List[int]]:
+        
+#leetcode submit region end(Prohibit modification and deletion)
