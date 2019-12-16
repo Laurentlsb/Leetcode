bb5cfbe40ba0c8b805b3a2b3bcc92e8f58e446ed
Laurentlsb <laurentlsb06@gmail.com>
2019-12-16 14:35:24 -0600

    suspended

--- leetcode/editor/en/[102]Binary Tree Level Order Traversal.py
+++ leetcode/editor/en/[102]Binary Tree Level Order Traversal.py
@@ -32,9 +32,57 @@
 #         self.left = None
 #         self.right = None
 
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+"""
 class Solution:
     def levelOrder(self, root: TreeNode) -> List[List[int]]:
+        if not root:
+            return []
+        level = [root]
+        res = []
+        while level:
+            res.append([node.val for node in level])
+            temp = []
+            for node in level:
+                if node.left:
+                    temp.append(node.left)
+                if node.right:
+                    temp.append(node.right)
+            level = temp
+        return res
+"""
 
+# class Solution:
+#     def levelOrder(self, root):
+#         ans, level = [], [root]
+#         while root and level:
+#             ans.append([node.val for node in level])
+#             LRpair = [(node.left, node.right) for node in level] # 以元组的形式，按pair存储
+#             level = [leaf for LR in LRpair for leaf in LR if leaf] # 取一个pair，再迭代pair中的非None节点，组成新的一层level
+#         return ans
 
+# 12/16/2019
+class Solution:
+    def levelOrder(self, root: TreeNode) -> List[List[int]]:
+        if root is None:
+            return []
+        res = []
+        level = [root]
+        while level:
+            res.append([node.val for node in level])
+            temp = []
+            for node in level:
+                if node.left:
+                    temp.append(node.left)
+                if node.right:
+                    temp.append(node.right)
+            level = temp
+        return res
 
 #leetcode submit region end(Prohibit modification and deletion)
