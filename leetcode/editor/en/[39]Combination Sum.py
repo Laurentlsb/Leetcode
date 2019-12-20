b6cc1fdc711629d501dc5810cba132b2eedc9325
Laurentlsb <laurentlsb06@gmail.com>
2019-12-20 11:18:25 -0600

    submit

--- leetcode/editor/en/[39]Combination Sum.py
+++ leetcode/editor/en/[39]Combination Sum.py
@@ -36,6 +36,7 @@
 
 
 #leetcode submit region begin(Prohibit modification and deletion)
+# 12/20/2019
 class Solution:
     def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
         candidates.sort()
@@ -58,5 +59,22 @@ backtracking
 通过回溯candidates，不断用target - num，直到target==0时返回找到的组合
 递归的回溯：传参时不能改变原来得temp，否则无法回退到上一个阶段得到的临时组合
 """
-        
+
+# leetcode solution
+def combinationSum(self, candidates, target):
+    res = []
+    candidates.sort()
+    self.dfs(candidates, target, 0, [], res)
+    return res
+
+
+def dfs(self, nums, target, index, path, res):
+    if target < 0:
+        return  # backtracking
+    if target == 0:
+        res.append(path)
+        return
+    for i in xrange(index, len(nums)):
+        self.dfs(nums, target - nums[i], i, path + [nums[i]], res)
+
 #leetcode submit region end(Prohibit modification and deletion)
